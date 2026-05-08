7bf324c1d9902afa7825e777a2a71d4dddd2838b
sloper00 <sloper00@estudiantes.unileon.es>
2026-05-08 10:31:53 +0200

    psswd

--- supabase/migrations/002_Users.sql
+++ supabase/migrations/002_Users.sql
@@ -1,6 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid (),
     email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
     full_name TEXT NOT NULL,
     avatar_url TEXT,
     role TEXT NOT NULL CHECK (
