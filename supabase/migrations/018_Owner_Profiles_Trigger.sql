6f8aa8109ca6a5d69aa2709f71d1543cd4399e81
DiegoFuertes00 <dfuerl00@estudiantes.unileon.es>
2026-06-07 16:51:54 +0200

    fix(supabase):delete all the old migrations files and put the last version of the tables in the database

--- supabase/migrations/018_Owner_Profiles_Trigger.sql
+++
@@ -1,7 +0,0 @@
-DROP TRIGGER IF EXISTS update_owner_profiles_updated_at
-ON owner_profiles;
-
-CREATE TRIGGER update_owner_profiles_updated_at
-BEFORE UPDATE ON owner_profiles
-FOR EACH ROW
-EXECUTE FUNCTION update_updated_at_column();
